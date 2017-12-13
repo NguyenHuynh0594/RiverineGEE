89280350354d2ef8b36a8c1cbb557eac21b4e950
Nguyen Huynh <nguyenhuynh0594@gmail.com>
2017-12-12 23:56:10 -0600

    Update LANDSAT8.js

--- LANDSAT8.js
+++ LANDSAT8.js
@@ -52,8 +52,8 @@ var End = ee.Date('2003-3-31');
 var Riverine = ee.ImageCollection('LANDSAT/LC8_L1T_TOA_FMASK')
 .filterBounds(river)
 .filterDate(Start,End)
-//.sort('DATE_ACQUIRED',true);
 .sort('CLOUD_COVER',true);
+//.sort('DATE_ACQUIRED',true);
 
 //Define the visualization parameters.
 var vizParams = {bands: ['B4', 'B3', 'B2'], min: 0, max: 0.5,gamma: [0.95, 1.1, 1]};
@@ -89,10 +89,12 @@ for(var x=0; x<Size_of_Collection; x++)
   var mask = scored.select(['cloud']).lte(20);
   var image_masked = image.updateMask(mask);
   */
+  
   // Fmask classification values
   var FMASK_CLOUD = 4;
   var FMASK_CLOUD_SHADOW = 2;
   
+  //Cloud Masking
   var fmask = image.select('fmask');
   var cloudMask = fmask.neq(FMASK_CLOUD)
     .and(fmask.neq(FMASK_CLOUD_SHADOW));
@@ -122,10 +124,34 @@ for(var x=0; x<Size_of_Collection; x++)
 //S--------------------------------------------------------------------------
 for(var x = 0; x<Size_of_Collection; x++)
 {
-	var name_of_Sat_Image = 'Satellite_Image_' + x;
-	var name_of_Cloud_Masked_Image = 'Cloud_Masked_Image_' + x;
-	var name_of_NDWI_Image = 'NDWI_Image_With_CloudMask_' + x;
-	var name_of_NDWI_Without_CloudMask_Image = 'NDWI_Image_Without_CloudMask_' + x;
+  //Print out date for each images and format the name of images
+  var date = new Date(ee.Number(Sat_Images_List[x].get('system:time_start')).getInfo());
+  print('Image ' + x + ' Timestamp: ', date); // ee.Date
+  var year = date.getFullYear().toString();
+  var month = (date.getMonth()+1).toString();
+  var day = date.getDate().toString();
+  var hour = (date.getHours()+5).toString();
+  var minute = date.getMinutes().toString();
+  var second = date.getSeconds().toString();
+
+  //Making sure everything other than year has at least 2 digits
+  if(month.length<2)
+    month = "0"+month;
+  if(day.length<2)
+    day = "0"+day;
+  if(hour.length<2)
+    hour = "0"+hour;
+  if(minute.length<2)
+    minute = "0"+minute;
+  if(second.length<2)
+    second = "0"+second;
+  var outputName = year+month+day+hour+minute+second;
+  
+  //Adding images to map
+	var name_of_Sat_Image = 'Sat_Image_' + x + "_" + outputName;
+	var name_of_Cloud_Masked_Image = 'CM_Sat_Image_' + x + "_" + outputName;
+	var name_of_NDWI_Image = 'NDWI_Image_CM_' + x + "_" + outputName;
+	var name_of_NDWI_Without_CloudMask_Image = 'NDWI_Image_WO_CM_' + x + "_" + outputName;
 	Map.addLayer(Sat_Images_List[x], vizParams, name_of_Sat_Image);
 	Map.addLayer(Sat_Cloud_Masked_Images_List[x], vizParams, name_of_Cloud_Masked_Image);
 	Map.addLayer(NDWI_Images_List[x], ndwiViz, name_of_NDWI_Image);
@@ -135,7 +161,7 @@ for(var x = 0; x<Size_of_Collection; x++)
   {
     Export.image.toDrive({
       image: Sat_Images_List[x].select(['B1', 'B2', 'B3', 'B4','B5', 'B6', 'B7', 'B8', 'B9']),
-      description: folderName + '_'  + name_of_Sat_Image,
+      description: name_of_Sat_Image,
       folder: folderName,
       scale: exportScale,
       region : AOI,
@@ -143,7 +169,7 @@ for(var x = 0; x<Size_of_Collection; x++)
     });
     Export.image.toDrive({
       image: Sat_Cloud_Masked_Images_List[x].select(['B1', 'B2', 'B3', 'B4','B5', 'B6', 'B7', 'B8', 'B9']),
-      description: folderName + '_'  + name_of_Cloud_Masked_Image,
+      description: name_of_Cloud_Masked_Image,
       folder: folderName,
       scale: exportScale,
       region : AOI,
@@ -151,7 +177,7 @@ for(var x = 0; x<Size_of_Collection; x++)
     });
     Export.image.toDrive({
       image: NDWI_Images_List[x].visualize(ndwiViz),
-      description: folderName + '_'  + name_of_NDWI_Image,
+      description: name_of_NDWI_Image,
       folder: folderName,
       scale: exportScale,
       region : AOI,
@@ -159,16 +185,12 @@ for(var x = 0; x<Size_of_Collection; x++)
     });
     Export.image.toDrive({
       image: NDWI_Images_List_WithOut_Cloud_Mask[x].visualize(ndwiViz),
-      description: folderName + '_'  + name_of_NDWI_Without_CloudMask_Image,
+      description: name_of_NDWI_Without_CloudMask_Image,
       folder: folderName,
       scale: exportScale,
       region : AOI,
       skipEmptyTiles : true
     });
   }
-  
-  //Print out date for each images
-  var date = ee.Date(Sat_Images_List[x].get('system:time_start'));
-  print('Image ' + x + ' Timestamp: ', date); // ee.Date
 }	
 //E--------------------------------------------------------------------------
